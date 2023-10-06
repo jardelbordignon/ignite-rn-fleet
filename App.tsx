96f43b6b595ceb1a87ca174faadeda1720ad4174
Jardel Bordignon <jardel1101@gmail.com>
2023-10-06 08:51:13 -0300

    Adicionando o Styled Components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components/native'
+
 import { SignIn } from 'src/views/screens/account/sign-in';
+import theme from 'src/theme';
 
 export default function App() {
   return (
-    <SignIn />
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
   )
 }
