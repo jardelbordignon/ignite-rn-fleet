a22a954e6e5108157554d045d25ad207ceb5017b
Jardel Bordignon <jardel1101@gmail.com>
2023-10-06 08:17:17 -0300

    Criando projeto

---
+++ src/views/screens/account/sign-in/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import s from './styles'
+
+export function SignIn() {
+  return (
+    <View style={s.container}>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
