a87714918c37564d3b445074edce2c2386ef5e74
Jardel Bordignon <jardel1101@gmail.com>
2023-10-07 07:55:38 -0300

    Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { StatusBar } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components/native'
 
-import { SignIn } from 'src/views/screens/account/sign-in';
-import theme from 'src/theme';
+import { Loading } from 'src/views/components/loading'
+import { SignIn } from 'src/views/screens/account/sign-in'
+import theme from 'src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  if (!fontsLoaded) return <Loading/>
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor='transparent' translucent />
       <SignIn />
     </ThemeProvider>
   )
---
+++ src/views/components/loading/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function Loading() {
+  return (
+    <S.root>
+      <S.loadingIndicator />
+    </S.root>
+  )
+}
