6b239dc01ec6113e66e7391798f2fb04d67bd7f5
Jardel Bordignon <jardel1101@gmail.com>
2023-10-12 11:22:52 -0300

    Carregando dados do usuário

--- src/views/components/home/header/index.tsx
+++ src/views/components/home/header/index.tsx
@@ -1,17 +1,20 @@
+import { useUser } from '@realm/react'
 import { Pressable } from 'react-native'
 
 import * as S from './styles'
 
 export function HomeHeader() {
+  const user = useUser()
+
   return (
     <S.root>
       <S.picture
-        source={{ uri: 'https://github.com/jardelbordignon.png' }}
+        source={{ uri: user?.profile.pictureUrl }}
         placeholder="T184i9j]j]00ayfQ~qj[ay9Fayfk" // https://blurha.sh/
       />
       <S.greeting>
         <S.message>Olá</S.message>
-        <S.name>Jardel</S.name>
+        <S.name>{user?.profile.name}</S.name>
       </S.greeting>
 
       <Pressable>
