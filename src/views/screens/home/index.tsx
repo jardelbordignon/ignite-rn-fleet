612971e13fc423cc1c6a2e7359ec6329f44d3e2f
Jardel Bordignon <jardel1101@gmail.com>
2023-10-16 21:25:42 -0300

    Exibindo dados do banco

--- src/views/screens/home/index.tsx
+++ src/views/screens/home/index.tsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react'
+
 import type { NavigationProps } from 'src/types/navigation'
+import { useQuery } from 'src/libs/realm'
+import { Historic } from 'src/libs/realm/schemas'
 import { CarStatus } from 'src/views/components/car/status'
 import { HomeHeader } from 'src/views/components/home/header'
 import * as S from './styles'
 
 export function Home({ navigation }: NavigationProps) {
+  const historic = useQuery(Historic)
+
   const handleRegisterMovement = () => {
     navigation.navigate('departure')
   }
+
+  const fetchVehicle = () => {
+    console.log(historic)
+  }
+
+  useEffect(fetchVehicle, [])
 
   return (
     <S.root>
