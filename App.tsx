5e1980f4692b165049f3f6c0506019339d13dc01
Jardel Bordignon <jardel1101@gmail.com>
2023-10-18 21:32:28 -0300

    NetInfo

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import {
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from 'styled-components/native'
+import { useNetInfo } from '@react-native-community/netinfo'
 
 import { REALM_APP_ID } from '@env'
 import { RealmProvider, syncConfiguration } from 'src/libs/realm'
@@ -18,6 +19,7 @@ import { SignIn } from 'src/views/screens/account/sign-in'
 import theme from 'src/theme'
 
 export default function App() {
+  const netInfo = useNetInfo()
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   if (!fontsLoaded) return <Loading />
@@ -26,7 +28,9 @@ export default function App() {
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
         <SafeAreaProvider style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_800 }}>
-          <TopMessage title="Você está off-line" icon={WifiSlash} />
+          {!netInfo.isConnected && (
+            <TopMessage title="Você está off-line" icon={WifiSlash} />
+          )}
           <StatusBar backgroundColor="transparent" translucent />
           <UserProvider fallback={SignIn}>
             <RealmProvider sync={syncConfiguration} fallback={Loading}>
