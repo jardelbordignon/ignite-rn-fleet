fcf1342f538243f308199069020920df9b636712
Jardel Bordignon <jardel1101@gmail.com>
2023-10-20 02:31:17 -0300

    Utilizando a API Key

---
+++ app.config.js
@@ -0,0 +1,53 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY
+
+module.exports = {
+  expo: {
+    name: 'ignitefleet',
+    slug: 'ignitefleet',
+    scheme: 'ignitefleet',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#202024',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'dev.bordignon.ignitefleet',
+      config: {
+        googleMapsApiKey,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#202024',
+      },
+      package: 'dev.bordignon.ignitefleet',
+      config: {
+        googleMaps: {
+          apiKey: googleMapsApiKey,
+        },
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+  plugins: [
+    [
+      'expo-location',
+      {
+        locationAlwaysAndWhenInUsePermission:
+          'Allow $(PRODUCT_NAME) to use your location.',
+      },
+    ],
+  ],
+}
